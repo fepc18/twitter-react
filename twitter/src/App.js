b67661b77e2fcbf0153d9e733c1085a28b233bf1
fepc18 <fepc18@gmail.com>
2023-05-10 15:22:51 -0500

    react bootstrap

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
+import { Alert, Button } from 'react-bootstrap';
 
+export default function App() {
+  const { user, setUser } = useState(null);
 
-function App() {
+  //return login page if user is not logged in
+
+  //return <div>{user? <h1>Estas logeado</h1> : <h1>No estas logeado</h1>   }</div>;
+
   return (
     <div>
-     <h1>App</h1>
-    </div>
+      <Button variant="primary">Primary</Button>
+      <Alert variant="danger">
+          This is a alert—check it out!
+        </Alert>
+       </div>
   );
+
 }
 
-export default App;
+
