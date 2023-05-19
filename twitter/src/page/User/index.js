6f888deb5c8f5ef74e5aa7b20709d5b1cf3e0dbb
fepc18 <fepc18@gmail.com>
2023-05-19 16:33:20 -0500

    struct user page

--- twitter/src/page/User/index.js
+++ twitter/src/page/User/index.js
@@ -1,11 +1,33 @@
-
-
 import React from 'react'
+import { Button,Spinner } from 'bootstrap'
+import BasicLayout from '../../layout/BasicLayout'
 
 import './User.scss'
 
-export default function User() {
+
+export default function User(props) {
+  const {setRefreshCheckLogin} = props
   return (
-    <div>Welcomen</div>
+    <BasicLayout className="user" setRefreshCheckLogin={setRefreshCheckLogin} >
+      <div className="user__title">
+        <h2>Felipe Esteban Pabon</h2>
+      </div>
+      <div className="user__container">
+        <div className="user__container__info">
+          <h3>Datos de usuario</h3>
+          <div className="user__container__info__item">
+            <p>Nombre:</p>
+            <p>Nombre Apellido</p>
+          </div>
+          <div className="user__container__info__item">
+            <p>Email:</p>
+           </div>
+          <div className="user__container__info__item">
+            <p>Fecha de nacimiento:</p>
+            <p>DD/MM/YYYY</p>
+          </div>
+        </div>
+      </div>
+    </BasicLayout>
   )
 }
