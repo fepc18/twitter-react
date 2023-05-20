ec62ad81eac5955b718c4dcd7073714ece8d308c
fepc18 <fepc18@gmail.com>
2023-05-20 11:28:44 -0500

    get Id from url

--- twitter/src/page/User/index.js
+++ twitter/src/page/User/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Button,Spinner } from 'bootstrap'
+import { Button, Spinner } from 'bootstrap'
+import {useParams} from 'react-router-dom';
+
 import BasicLayout from '../../layout/BasicLayout'
+
 
 import './User.scss'
 
 
 export default function User(props) {
-  const {setRefreshCheckLogin} = props
+  const { setRefreshCheckLogin } = props
+  const { id } = useParams();
+  console.log(id);
   return (
     <BasicLayout className="user" setRefreshCheckLogin={setRefreshCheckLogin} >
       <div className="user__title">
@@ -21,7 +26,7 @@ export default function User(props) {
           </div>
           <div className="user__container__info__item">
             <p>Email:</p>
-           </div>
+          </div>
           <div className="user__container__info__item">
             <p>Fecha de nacimiento:</p>
             <p>DD/MM/YYYY</p>
@@ -31,3 +36,6 @@ export default function User(props) {
     </BasicLayout>
   )
 }
+
+
+//export default withRouter(User);//withRouter es un HOC que permite pasar las propiedades de react-router-dom a un componente que no es una ruta
