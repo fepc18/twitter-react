8c5b2544b9c2f46563084fe8de17f81b4173f52f
fepc18 <fepc18@gmail.com>
2023-05-20 20:14:12 -0500

    Modal for to edit profile

--- twitter/src/components/User/BannerAvatar/index.js
+++ twitter/src/components/User/BannerAvatar/index.js
@@ -1,26 +1,30 @@
 import './BannerAvatar.scss'
-import React from 'react'
+import React, { useState } from 'react'
 import AvatarNotFound from '../../../assets/png/avatar-no-found.png';
 import { API_HOST } from '../../../utils/constants';
 import { Button } from 'react-bootstrap';
 
+import ConfigModal from '../../Modal/ConfigModal';
 
 export default function BannerAvatar(props) {
   const { user, loggedUser } = props;
   const bannerUrl = user?.banner ? `${API_HOST}/getbanner?id=${user.id}` : null;
   const avatarUrl = user?.avatar ? `${API_HOST}/getavatar?id=${user.id}` : AvatarNotFound;
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div className='banner-avatar' style={{ backgroundImage: `url('${bannerUrl}')` }}>
       <div className='avatar' style={{ backgroundImage: `url('${avatarUrl}')` }} />
       {user && (
         <div className='options'>
-          {user.id === loggedUser._id && <Button>Editar Perfil</Button>}
+          {user.id === loggedUser._id && <Button onClick={()=>setShowModal(true)}>Editar Perfil</Button>}
           {loggedUser._id !== user.id && <Button>Seguir</Button>}
         </div>
 
       )}
-
+      <ConfigModal show={showModal} setShow={setShowModal} title="Editar Perfil">
+        <h1>Formulario de edición</h1>
+      </ConfigModal>
     </div>
 
   )
