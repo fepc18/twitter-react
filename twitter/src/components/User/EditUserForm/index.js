d79b95deb9e972050109d96efb1a528b80090290
fepc18 <fepc18@gmail.com>
2023-05-22 06:16:32 -0500

    fill in fields with user's data

--- twitter/src/components/User/EditUserForm/index.js
+++ twitter/src/components/User/EditUserForm/index.js
@@ -1,5 +1,5 @@
 import './EditUserForm.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import { es } from 'date-fns/locale';
@@ -11,6 +11,7 @@ export default function EditUserForm(props) {
 
     const { user, setShowModal } = props;
     const { name, lastName, biography, location, website } = user;
+    const [formData, setFormData] = useState(initialValueForm(user));
 
     const onSubmit = e => {
         e.preventDefault();
@@ -24,24 +25,24 @@ export default function EditUserForm(props) {
                 <Form.Group>
                     <Row>
                         <Col>
-                            <Form.Control type="text" placeholder="Nombre" defaultValue={user.name} />
+                            <Form.Control type="text" placeholder="Nombre" defaultValue={formData.name} />
                         </Col>
                         <Col>
-                            <Form.Control type="text" placeholder="Apellidos" defaultValue={user.lastName} />
-                        </Col>
-                    </Row>
-                </Form.Group>
-                <Form.Group>
-                    <Row>
-                        <Col>
-                            <Form.Control as="textarea" row="3" placeholder="Agrega tu biografía" defaultValue={user.biography} />
+                            <Form.Control type="text" placeholder="Apellidos" defaultValue={formData.lastName} />
                         </Col>
                     </Row>
                 </Form.Group>
                 <Form.Group>
                     <Row>
                         <Col>
-                            <Form.Control type="text" placeholder="Sitio Web" defaultValue={user.website} />
+                            <Form.Control as="textarea" row="3" placeholder="Agrega tu biografía" defaultValue={formData.biography} />
+                        </Col>
+                    </Row>
+                </Form.Group>
+                <Form.Group>
+                    <Row>
+                        <Col>
+                            <Form.Control type="text" placeholder="Sitio Web" defaultValue={formData.website} />
                         </Col>
                     </Row>
                 </Form.Group>
@@ -51,8 +52,9 @@ export default function EditUserForm(props) {
                             <DatePicker
                                 placeholder="Fecha de nacimiento"
                                 locale={es}
-                                selected={new Date(user.birthdate)}
+                                selected={new Date(formData.birthdate)}
                                 onChange={date => console.log(date)}
+                                
                             />
                         </Col>
                     </Row>
@@ -61,7 +63,7 @@ export default function EditUserForm(props) {
                 <Form.Group>
                     <Row>
                         <Col>
-                            <Form.Control type="text" placeholder="Ubicación" defaultValue={user.location} />
+                            <Form.Control type="text" placeholder="Ubicación" defaultValue={formData.location} />
                         </Col>
                     </Row>
                 </Form.Group>
@@ -83,4 +85,15 @@ export default function EditUserForm(props) {
             </Form>
         </div>
     )
+}
+
+function initialValueForm(user) {
+    return {
+        name: user.name ? user.name : "",
+        lastName: user.lastname ? user.lastname : "",
+        biography: user.biography ? user.biography : "",
+        location: user.location ? user.location : "",
+        website: user.website ? user.website : "",
+        birthdate: user.birthdate ? user.birthdate : "",
+    }
 }
