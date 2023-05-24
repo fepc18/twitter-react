8d27db9582847e4694454626dde803a0a1cd4e64
fepc18 <fepc18@gmail.com>
2023-05-24 07:18:35 -0500

    struct user's page

--- twitter/src/page/Users/index.js
+++ twitter/src/page/Users/index.js
@@ -1,13 +1,25 @@
-
+import { Spinner, ButtonGroup, Button } from 'react-bootstrap'
 import BasicLayout from '../../layout/BasicLayout'
 import './Users.scss'
 import React from 'react'
 
 
-export default function Users() {
-  return (
-    <BasicLayout className="users">
-        <h2>Users</h2>
-    </BasicLayout>
-  )
+export default function Users(props) {
+    const { setRefreshCheckLogin } = props;
+    return (
+        <BasicLayout
+            className="users"
+            setRefreshCheckLogin={setRefreshCheckLogin}
+            title="Users"
+        >
+            <div className="users__title">
+                <h2>Users</h2>
+                <input type="text" placeholder="Search user" />
+            </div>
+            <ButtonGroup className="users__options" >
+                <Button>Siguiendo</Button>
+                <Button>Nuevos</Button>
+            </ButtonGroup>
+        </BasicLayout>
+    )
 }
