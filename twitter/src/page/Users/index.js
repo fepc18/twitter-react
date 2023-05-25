fc7fcbe3fe60d6f95542b5d378fc625d1bf693ae
fepc18 <fepc18@gmail.com>
2023-05-25 15:55:27 -0500

    pagination of user TODO: loading while upload

--- twitter/src/page/Users/index.js
+++ twitter/src/page/Users/index.js
@@ -1,5 +1,5 @@
 import { Spinner, ButtonGroup, Button } from 'react-bootstrap'
-import { useSearchParams,useNavigate } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import queryString from 'query-string';
 import { isEmpty } from 'lodash';
 import { useDebouncedCallback } from 'use-debounce';
@@ -13,46 +13,67 @@ import './Users.scss'
 
 export default function Users(props) {
     const { setRefreshCheckLogin } = props;
-    const [users, setUsers] = useState(null);
-    
+    const [users, setUsers] = useState([]);
+
     const [searchParams] = useSearchParams();
     const params = useUsersQuery(Object.fromEntries([...searchParams]));
     const [typeUser, setTypeUser] = useState(params.type || "follow");
     const navigate = useNavigate();
-    
-    const onSearch=useDebouncedCallback((value)=>{
-        setUsers(null);
-        navigate(`/users?type=${typeUser}&page=1&search=${value}`);
-        console.log(value);
-        window.location.reload(); //reload page to get new params
-    },500)
-
-
+    const [btnLoading, setBtnLoading] = useState(false);
+    const [search, setSearch] = useState(params.search || "");
+    const [page, setPage] = useState(params.page || 1);
+    const onSearch = useDebouncedCallback((value) => {
+        setUsers([]);
+        navigate(`/users?type=${typeUser}&page=1&search=${value}`);   
+        setSearch(value);     
+      //  window.location.reload(); //reload page to get new params
+    }, 500)
 
     //get users from api send type user and page
-    useEffect(() => {       
+    useEffect(() => {
         getFollowsApi(queryString.stringify(params)).then(response => {
-            if (isEmpty(response)) 
-                setUsers([]);
-            else
-                setUsers(response);
-        })
-    }, [typeUser])
+            console.log(response);          
+            if (params.page === 1) {             
+                if (isEmpty(response) || response.length === 0){                                 
+                    setUsers([]);
+                }                    
+                else
+                    setUsers(response);
+            } else {
+                if (isEmpty(response)) {
+                    setBtnLoading(0);
+                } else {
+                   
+                    setUsers([...users, ...response]);
+                    setBtnLoading(false);
+                }
+            }
+
+        }).catch(() => {
+            setUsers([]);
+        });
+
+    }, [search,page,typeUser])
 
     //change type user
     const onChangeType = (type) => {
-        setUsers(null);
+        setUsers([]);
         if (type === "new") {
             setTypeUser("new");
         } else {
             setTypeUser("follow");
         }
+        navigate(`/users?type=${type}&page=1`);        
+     
 
-        navigate(`/users?type=${type}&page=1`);     
-
-    }  
-   
-    
+    }
+    //more data to load using page, type user and search
+    const moreData = () => {
+        setBtnLoading(true);
+        const newPage = parseInt(params.page) + 1;
+        setPage(newPage);
+        navigate(`/users?type=${typeUser}&page=${newPage}&search=${search}`);
+    }
 
     return (
         <BasicLayout
@@ -62,17 +83,17 @@ export default function Users(props) {
         >
             <div className="users__title">
                 <h2>Users</h2>
-                <input type="text" 
-                placeholder="Search user"
-                onChange={e=>onSearch(e.target.value)}
-                 />
+                <input type="text"
+                    placeholder="Search user"
+                    onChange={e => onSearch(e.target.value)}
+                />
             </div>
             <ButtonGroup className="users__options" >
                 <Button variant={typeUser === "follow" && ("primary" || "secundary")}
-                    onClick={()=>onChangeType("follow")}>
-                        Siguiendo</Button>
-                <Button variant={typeUser === "new" && ("primary" || "secundary")} 
-                    onClick={()=>onChangeType("new")}>Nuevos</Button>
+                    onClick={() => onChangeType("follow")}>
+                    Siguiendo</Button>
+                <Button variant={typeUser === "new" && ("primary" || "secundary")}
+                    onClick={() => onChangeType("new")}>Nuevos</Button>
             </ButtonGroup>
             {!users ? (
                 <div className="users__loading">
@@ -80,7 +101,23 @@ export default function Users(props) {
                     Loading users...
                 </div>
             ) : (
-                <ListUsers users={users} />
+                <>
+                    <ListUsers users={users} />
+                    <Button className="load-more" onClick={moreData} disabled={btnLoading}>
+                        {!btnLoading ? (
+                            btnLoading !== 0 ? "There are no more users" : "Load more users"
+                        ) : (
+                            <Spinner
+                                as="span"
+                                animation="grow"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                        )}
+                    </Button>
+
+                </>
             )}
 
 
