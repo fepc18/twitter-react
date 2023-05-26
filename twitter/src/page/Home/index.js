da94311c70826e644920cec81677fd809e3f3e01
fepc18 <fepc18@gmail.com>
2023-05-25 20:16:56 -0500

    struct tweet's page

--- twitter/src/page/Home/index.js
+++ twitter/src/page/Home/index.js
@@ -3,11 +3,15 @@ import './Home.scss'
 import BasicLayout from '../../layout/BasicLayout'
 
 export default function Home(props) {
-    const {setRefreshCheckLogin} = props;
+    const { setRefreshCheckLogin } = props;
     return (
 
         <BasicLayout className="home" setRefreshCheckLogin={setRefreshCheckLogin} >
-            <h1>Estamos en Home</h1>
+            <div className="home__title">
+                <h2>Inicio</h2>
+            </div>
+            <p>Lista de Tweets</p>
+            <p>Cargar mas Tweets</p>
         </BasicLayout>
 
 
